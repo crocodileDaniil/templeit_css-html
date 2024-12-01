051a6400b67f67c8add3317c806550dd812ac91f
CrocodileDaniil <karasev_dan_an@mail.ru>
2024-12-01 21:50:13 +0300

    первый коммит и первая настройка

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const cleanCSS = require("gulp-clean-css");
+const browserSync = require("browser-sync").create();
+
+// Пути к SCSS и CSS
+const paths = {
+    scss: "src/scss/**/*.scss", // Все файлы .scss в папке src/scss
+    css: "dist/css"             // Папка для выходных файлов
+};
+
+// Задача для компиляции SCSS
+gulp.task("scss", function () {
+    return gulp.src(paths.scss) // Берём файлы SCSS
+        .pipe(sass().on("error", sass.logError)) // Компилируем в CSS и обрабатываем ошибки
+        .pipe(cleanCSS()) // Минифицируем CSS
+        .pipe(gulp.dest(paths.css)); // Кладём CSS в папку dist/css
+});
+
+// Задача для отслеживания изменений
+gulp.task("watch", function () {
+    gulp.watch(paths.scss, gulp.series("scss")); // При изменении SCSS запускаем задачу scss
+});
+
+gulp.task("serve", function () {
+  browserSync.init({
+      server: "./dist"
+  });
+
+  gulp.watch(paths.scss, gulp.series("scss")).on("change", browserSync.reload);
+});
+
+// Задача по умолчанию
+gulp.task("default", gulp.series("scss", "watch"));
